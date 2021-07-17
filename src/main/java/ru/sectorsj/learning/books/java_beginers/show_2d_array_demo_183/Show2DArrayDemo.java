c43f9ff1f4b4f95f0963f591c8229ca9b28c9ac7
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2021-07-17 19:49:20 +0300

    create Show2DArrayDemo

--- src/main/java/ru/sectorsj/learning/books/java_beginers/show_2d_array_demo_183/Show2DArrayDemo.java
+++ src/main/java/ru/sectorsj/learning/books/java_beginers/show_2d_array_demo_183/Show2DArrayDemo.java
@@ -1,4 +1,21 @@
 package ru.sectorsj.learning.books.java_beginers.show_2d_array_demo_183;
 
 public class Show2DArrayDemo {
+	static void show (int[][] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums[i].length; j++) {
+				System.out.print(nums[i][j] + " ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[][] alpha = {{1, 2, 3}, {4, 5, 6}};
+		int[][] bravo = {{1, 2, 3}, {4, 5}, {6, 7, 8}};
+		System.out.println("Массив alpha:");
+		show(alpha);
+		System.out.println("Массив bravo:");
+		show(bravo);
+	}
 }
